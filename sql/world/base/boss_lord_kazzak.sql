e95dd98de5f6cbf6f52080d82660562fb8ee2079
Gabriel Comeau <gabriel-comeau@users.noreply.github.com>
2026-06-27 17:15:10 -0400

    Missed some columns on lord K

--- sql/world/base/boss_lord_kazzak.sql
+++ sql/world/base/boss_lord_kazzak.sql
@@ -21,8 +21,8 @@ INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Lan
 (12397, 8, 0, 'Kazzak is supreme!', 16, 0, 0, 0, 0, 0, 0, 0, 'kazzak SAY_SUPREME_VANILA');
 
 DELETE FROM `creature` WHERE `guid` = 156950 AND `id` = 12397;
-INSERT INTO `creature` (`guid`, `id`, `id2`, `id3`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `wander_distance`, `spawntimesecs`, `MovementType`) VALUES
-(156950, 12397, 0, 0, 0, -12275.6, -2524.26, 3.79202, 1.69492, 259200, 604800, 1);
+INSERT INTO `creature` (`guid`, `id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `wander_distance`, `spawntimesecs`, `MovementType`) VALUES
+(156950, 12397, 0, -12275.6, -2524.26, 3.79202, 1.69492, 259200, 604800, 1);
 
 DELETE FROM `reference_loot_template` WHERE `Entry` = 60000;
 INSERT INTO `reference_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
